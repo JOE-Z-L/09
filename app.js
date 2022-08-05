fea6394b98ea363752a1e76cf59a6216e0b181d7
JOE-Z-L <josiel.gp@gmail.com>
2022-08-05 22:37:11 +0100

    xss sanitize pollution

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -31,6 +34,26 @@ app.use('/api', limiter);
 app.use(
   express.json({
     limit: '10kb'
+  })
+);
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data satinization against XSS
+app.use(xss());
+
+// Prevent parameter pollution (limpa a query string)
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
   })
 );
 
