02f98f8b9d65ba6f44ae776700083f0c20c768a6
JOE-Z-L <josiel.gp@gmail.com>
2022-08-09 19:20:37 +0100

    refactor auth to reviewRoutes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,24 +1,27 @@
 const express = require('express');
-const authController = require('./../controllers/authController');
 const reviewController = require('./../controllers/reviewController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
 router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
   );
 
 router
-  .route('/:d')
+  .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
   .delete(
     authController.restrictTo('user', 'admin'),
     reviewController.deleteReview
