680ee897cc6572f2f842aff84b57352f998cf225
JOE-Z-L <josiel.gp@gmail.com>
2022-08-12 07:51:49 +0100

    html teste inicial com pug

--- app.js
+++ app.js
@@ -74,7 +74,10 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Josiel'
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
