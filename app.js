dfd7c1468aedbd333a6f2609fe94c042331cdb23
JOE-Z-L <josiel.gp@gmail.com>
2022-08-13 08:33:39 +0100

    view controller and mount view routes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const viewRoute = require("./routes/viewRoutes");
 
 const app = express();
 
@@ -73,25 +74,8 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get("/", (req, res) => {
-  res.status(200).render("base", {
-    tour: "The Forest Hiker",
-    user: "Josiel"
-  });
-});
 
-app.get("/overview", (req, res) => {
-  res.status(200).render("overview", {
-    title: "All Tours"
-  });
-});
-
-app.get("/tour", (req, res) => {
-  res.status(200).render("tour", {
-    title: "The Forest Hiker"
-  });
-});
-
+app.use("/", viewRoute);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
