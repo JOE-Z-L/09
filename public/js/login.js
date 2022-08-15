4733724093ef6fd99f5f0846fc9d6a0232cda1a1
JOE-Z-L <josiel.gp@gmail.com>
2022-08-15 15:45:51 +0100

    Logout users implementation

--- public/js/login.js
+++ public/js/login.js
@@ -24,4 +24,19 @@ export const login = async (email, password) => {
   }
 };
 
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: "GET",
+      url: "http://127.0.0.1:3000/api/v1/users/logout"
+    });
+
+    if ((res.data.status = "success")) {
+      location.reload(true);
+    }
+  } catch (err) {
+    showAlert("error", "Error logging out! Try again!");
+  }
+};
+
 //https://codepen.io/hoskinsconsulting/project/editor/ZLMaRz - signUp example
